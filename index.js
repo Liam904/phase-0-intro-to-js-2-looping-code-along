1aea71053659fd0778b2464be05e9f1e8e2ebbfa
Liam <lw433891@gmail.com>
2024-03-24 14:56:17 +0300

    'done'

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
+  
+  countDown(10);
+
+
+  function writeCards(names, eventName) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+  }
+  
+  // Example usage
+  let messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+  console.log(messages);
